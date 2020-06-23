d574a60f8d8c1be8fe89381ff3f5b00247bbcdce
SamPom100 <sampom100@gmail.com>
2020-06-23 17:59:26 -0500

    added main method

--- main.py
+++ main.py
@@ -3,36 +3,41 @@ import glob
 from process import getDominantColor
 import os
 
-COLORS = [
-    "red",
-    "orange",
-    "yellow",
-    "spring green",
-    "green",
-    "turquoise",
-    "cyan",
-    "ocean",
-    "blue",
-    "violet",
-    "magenta",
-    "raspberry",
-    "black",
-    "white",
-]
 
-if not os.path.exists('process'):
-    os.makedirs('process')
+def main():
+    COLORS = [
+        "red",
+        "orange",
+        "yellow",
+        "spring green",
+        "green",
+        "turquoise",
+        "cyan",
+        "ocean",
+        "blue",
+        "violet",
+        "magenta",
+        "raspberry",
+        "black",
+        "white",
+    ]
 
-if not os.path.exists('output'):
-    os.makedirs('output')
+    if not os.path.exists('process'):
+        os.makedirs('process')
 
-for item in COLORS:
-    if not os.path.exists('output/'+item):
-        os.makedirs('output/'+item)
+    if not os.path.exists('output'):
+        os.makedirs('output')
 
-for filename in glob.glob('process/*'):
-    try:
-        im = Image.open(filename)
-        getDominantColor(im, str(filename[8:]))
-    except:
-        print(str(filename) + ' is not an image')
+    for item in COLORS:
+        if not os.path.exists('output/'+item):
+            os.makedirs('output/'+item)
+
+    for filename in glob.glob('process/*'):
+        try:
+            im = Image.open(filename)
+            getDominantColor(im, str(filename[8:]))
+        except:
+            print(str(filename) + ' is not an image')
+
+
+main
